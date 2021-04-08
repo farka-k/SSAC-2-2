7dd8259b0d46e87b8c9612f0305f5df8f6dbd7a1
farka-k <32349691+farka-k@users.noreply.github.com>
2021-04-08 09:38:41 +0900

    split vue and removed in main

--- shadow-ai-front/src/components/ShadowAiFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <v-footer app dark padless>
-        <v-row>
-            <v-col class="text-center">
-                SSAC AI Class Team-2 2nd Project Shadow.Ai 
-            </v-col>
-        </v-row>
-    </v-footer>
-</template>
